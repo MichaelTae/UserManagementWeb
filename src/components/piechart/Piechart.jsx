60a0337a5c7242632a34950755065e5d5745e68b
Michael S <Michael.Stojanovic@hotmail.com>
2023-05-11 14:40:16 +0200

    added Piechart component

--- src/components/piechart/Piechart.jsx
+++ src/components/piechart/Piechart.jsx
@@ -42,48 +42,45 @@ border: '1px solid rgba(0,191,166,0.3)',
 	
 }));
 
-const Piechart = ({ data, title, color }) => {
-const {classes} = useStyles();
-    const chartData = data;
-	const [activeIndex, setActiveIndex] = useState(0);
-    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-	const onPieEnter = (_, index) => {
-		setActiveIndex(index);
-	};
+const Piechart = ({ data, title, dataKey, nameKey }) => {
+    const {classes} = useStyles();
+    const [activeIndex, setActiveIndex] = useState(0);
+    const COLORS =  ['#e9d8a6', '#0a9396', '#48bfe3', '#72efdd','#52b788','#eddcd2','#ddbea9'];
+    const onPieEnter = (_, index) => {
+        setActiveIndex(index);
+    };
     return(
         <>
-		<Text pos={'absolute'} sx={{zIndex:10}} ml={10} size={20} weight={600}>User Age distribution</Text>
-        <ResponsiveContainer width='100%' height='100%' className={classes.container}  >
-				<PieChart className={classes.chart}>
-					
-						<Pie
-							data={chartData}
-							dataKey={'quantity'}
-							nameKey={'ageSpan'}
-							cx='50%'
-							cy='53%'
-							innerRadius={'55%'}
-							outerRadius={'75%'}
-							blendStroke={true}
-							labelLine={false}
-							 activeIndex={activeIndex}
-							 activeShape={renderActiveShape}
-							onMouseEnter={onPieEnter}
-							
-							
-							label={({ percent }) => `${(percent * 100).toFixed(0)}%`}
-							minAngle={10}
-						>
-							{chartData?.map((entry, index) => (
-								<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-							))}
-						</Pie>
-						<Legend align='left' />
-                     </PieChart>
-                     </ResponsiveContainer>
-					 </>
+        <Text pos={'absolute'} sx={{zIndex:10}} ml={10} size={20} weight={600}>{title}</Text>
+        <ResponsiveContainer width='100%' height='100%' className={classes.container}>
+            <PieChart className={classes.chart}>
+                <Pie
+                    data={data}
+                    dataKey={dataKey}
+                    nameKey={nameKey}
+                    cx='50%'
+                    cy='53%'
+                    innerRadius={'55%'}
+                    outerRadius={'75%'}
+                    blendStroke={true}
+                    labelLine={false}
+                    activeIndex={activeIndex}
+                    activeShape={renderActiveShape}
+                    onMouseEnter={onPieEnter}
+                    label={({ percent }) => `${(percent * 100).toFixed(0)}%`}
+                    minAngle={10}
+                >
+                    {data?.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                </Pie>
+                <Legend align='left' />
+            </PieChart>
+        </ResponsiveContainer>
+        </>
     )
 };
+
 
 
 
