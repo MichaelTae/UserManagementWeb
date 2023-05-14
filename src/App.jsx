a4bf41487ee5371b55477d0389396f6103fe308a
Michael S <Michael.Stojanovic@hotmail.com>
2023-05-14 18:48:38 +0200

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ import Home from './pages/home/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ErrorPage from './pages/Errorpage'
 import Sidebar from './components/sidebar/Sidebar'
-import { useState } from 'react';
+import {useEffect, useState } from 'react';
+import PrivateRoutes from './helpers/auth/privateRoutes';
+import Login from './pages/login/login';
 
 function App() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(true);
   
 
- 
  
   return (
     <div className="app">
       <Router>
-      <Sidebar isOpen={sidebarOpen} onToggle={() => setSidebarOpen(!sidebarOpen)} />
+       <Sidebar isOpen={sidebarOpen} onToggle={() => setSidebarOpen(!sidebarOpen)} />
         <main className={`content ${sidebarOpen ? 'sidebar-open' : 'sidebar-closed'}`}>
         <div className="background">
    <span />
@@ -33,10 +34,13 @@ function App() {
    
 </div>
           <Routes>
+            <Route element={<PrivateRoutes/>} >
             <Route path='/' element={<Home isOpen={sidebarOpen} />} />
             <Route path='/users' element={<Users isOpen={sidebarOpen} />} />
             <Route path='/revenue' element={<Revenue isOpen={sidebarOpen}/>} />
             <Route path='*' element={<ErrorPage/>} />
+            </Route>
+            <Route element={<Login/>} path='/login' />
           </Routes>
         </main>
       </Router>
